22fcc19355f1419fb353ef8a46c08bc0897100ea
Michael Rodas <>
2020-04-30 20:22:00 +0200

    tool for measuring java object's size

---
+++ objectsize/src/main/java/poc/dto/JsonStore.java
@@ -0,0 +1,20 @@
+package poc.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class JsonStore {
+    @JsonIgnore
+    private Attributes attributes;
+    private List<Store> stores;
+
+    private static class Attributes {
+    }
+}
---
+++ objectsize/src/main/java/poc/dto/Store.java
@@ -0,0 +1,29 @@
+package poc.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Store {
+
+    private double latitude;
+    private double longitude;
+    private String uuid;
+    private String city;
+    private String postalCode;
+    private String street;
+    private String street2;
+    private String street3;
+    private String addressName;
+    private int complexNumber;
+    private boolean showWarningMessage;
+    private String todayOpen;
+    private String locationType;
+    private boolean collectionPoint;
+    private int sapStoreID;
+    private String todayClose;
+
+}
